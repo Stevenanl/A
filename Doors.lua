da6f64eb2abd890cb8029b31409b7f0d8e777c19
Stevenanl <115950740+Stevenanl@users.noreply.github.com>
2024-09-13 16:19:15 +0800

    update Doors.lua

--- Doors.lua
+++ Doors.lua
@@ -740,6 +740,11 @@ mt.__namecall = newcclosure(function(remote, ...)
 end);
 setreadonly(mt, true);
 Lighting.FogEnd = 100000;
+for i, v in pairs(Lighting:GetDescendants()) do
+	if v:IsA("Atmosphere") then
+		v:Destroy();
+	end
+end
 local ScreenGui = Instance.new("ScreenGui");
 ScreenGui.Parent = pg;
 ScreenGui.Name = "blephaha";
