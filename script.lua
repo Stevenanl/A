82d9787332d87d5c7936c0a5fcaf7b8f64b7b0b4
Stevenanl <115950740+Stevenanl@users.noreply.github.com>
2024-09-19 18:33:16 +0800

    update script.lua

--- script.lua
+++ script.lua
@@ -126,76 +126,6 @@ local function toggletimer()
 		end);
 	end
 end
-local function generate(url)
-	local image = url;
-	local resolutionX = 32;
-	local resolutionY = 32;
-	local grid = nil;
-	local s, e = pcall(function()
-		if game.Players.LocalPlayer.PlayerGui("MainGui"):FindFirstChild("PaintFrame"):FindFirstChild("Grid") then
-			grid = game.Players.LocalPlayer.PlayerGui.MainGui.PaintFrame.GridHolder.Grid;
-		elseif game.Players.LocalPlayer.PlayerGui("PaintFrame"):FindFirstChild("GridHolder"):FindFirstChild("Grid") then
-			grid = game.Players.LocalPlayer.PlayerGui.MainGui.PaintFrame.GridHolder.Grid;
-		else
-			warn("cannot execute script");
-			return;
-		end
-	end);
-	if e then
-		local s1, e1 = pcall(function()
-			grid = game.Players.LocalPlayer.PlayerGui.MainGui.PaintFrame.GridHolder.Grid;
-		end);
-		if e1 then
-			warn("cannot execute script");
-			return;
-		end
-	end
-	local h = game:GetService("HttpService");
-	function getjson(url)
-		local begin = game:HttpGet("https://f818fcf9-3b10-4c92-8176-0bef47a8421d.id.repl.co/get?url=" .. url);
-		if (begin == "the file size is too big!") then
-			return "fstb";
-		else
-			local json = h:JSONDecode(begin);
-			return json;
-		end
-	end
-	function import(url)
-		local pixels = getjson(url);
-		local cells = {};
-		local index = 1;
-		if (pixels == "fstb") then
-			send("error",("the file size exceeds three megabytes, " .. "to prevent people from crashing the vps, he have set" .. " the cap to amount. sorry for the inconvenience"));
-		else
-			grid["1"].BackgroundColor3 = Color3.fromRGB(pixels[1][1], pixels[1][2], pixels[1][3]);
-			for y = 1, resolutionX, 1 do
-				for x = 1, resolutionY, 1 do
-					pcall(function()
-						local pixel = pixels[index];
-						index = index + 1;
-						local r = pixels[index][1];
-						local g = pixels[index][2];
-						local b = pixels[index][3];
-						grid[tostring(index)].BackgroundColor3 = Color3.fromRGB(r, g, b);
-						table.insert(cells, pixel);
-					end);
-				end
-			end
-			pcall(function()
-				local pixel = pixels[index];
-				index = index + 1;
-				local r = pixels[index][1];
-				local g = pixels[index][2];
-				local b = pixels[index][3];
-				grid[tostring(index)].BackgroundColor3 = Color3.fromRGB(r, g, b);
-				table.insert(cells, pixel);
-			end);
-			send("done","finished importing, check the drawing grid");
-		end
-	end
-	import(image);
-	toggletimer()
-end
 local function create(option)
 	local Pixels = data[option];
 	local UI = game.Players.LocalPlayer.PlayerGui.MainGui.PaintFrame.GridHolder.Grid;
@@ -316,9 +246,6 @@ Visualstab:Toggle("Timer", false, function(t)
 end);
 Visualstab:Toggle("Delay", false, function(t)
 	delay = t;
-end);
-Artstab:Textbox("Generate from URL", true, function(link)
-	generate(link)
 end);
 Artstab:Dropdown("Select Animals Art", {"Cat","Cat1","Cat2","Cat3","Cat4","Cat5","Cat6","Cat7","Cat8","Cat9","Cat10","Cat11","Cat12","Cat13","Cat14","Cat15","Cat16","Cat17","Cat18","Cat19","Cat20","Cat21","Cat22","Cat23","Dog","Penguin"}, function(option)
 	create(option)
